a2d492ebb899acdbe958748f8aa47223b1ab6471
Ronald Kaiser <raios.catodicos@gmail.com>
2013-02-24 17:17:09 -0300

    fixing tyop

--- test.pl
+++ test.pl
@@ -15,7 +15,7 @@ diag("diag prints a message within test");
 like("know thyself", qr/thy/, "like = this is like that");
 unlike("know thyself", qr/knw/, "unlike = NOT like");
 
-cmp_ok("p1", 'ne', "p2", "cmp_ok = compare p1 with p2 using neq as operator");
+cmp_ok("p1", 'ne', "p2", "cmp_ok = compare p1 with p2 using ne as operator");
 
 my @foo = qw(foo bar);
 is_deeply(\@foo, \@foo, "is_deeply is used for complex datastructures comparisons");
